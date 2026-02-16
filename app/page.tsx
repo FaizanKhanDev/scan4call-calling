91fe1f4366b643641ecd95fe7b2b472ccbcaebe5
faizankhandev <faizan480khan@gmail.com>
2026-02-16 19:56:12 +0500

    FIXED: the asking number

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Calling from './component/Calling'
 
 export default function page() {
   return (
-    <div className="min-h-screen h-screen bg-[#07132C] overflow-hidden p-0 m-0">
+    <div className="min-h-screen  bg-[#07132C] overflow-hidden p-0 m-0">
       <Scan4CallContact />
       <Calling></Calling>
     </div>
