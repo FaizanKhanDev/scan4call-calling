bb29987fb4210c24bb4e140117b7ce044b76e432
faizankhandev <faizan480khan@gmail.com>
2026-02-21 19:59:31 +0500

    FIXED: te phone target numbeR

--- app/api/twiml/route.js
+++ app/api/twiml/route.js
@@ -17,7 +17,7 @@ export async function POST(req) {
 
     // Add the callerId here inside an object
     let number = `+${targetNumber}`
-    response.dial({ callerId: myTwilioNumber }, number);
+    response.dial({ callerId: myTwilioNumber }, targetNumber);
 
     return new Response(response.toString(), {
         headers: { 'Content-Type': 'text/xml' },
@@ -39,7 +39,7 @@ export async function GET(req) {
 
     let number = `+${targetNumber}`
 
-    response.dial({ callerId: myTwilioNumber }, number);
+    response.dial({ callerId: myTwilioNumber }, targetNumber);
 
     return new Response(response.toString(), {
         headers: { 'Content-Type': 'text/xml' },
