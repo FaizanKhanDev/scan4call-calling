dfad573dba68eb641566c54f6742c9c8bc40212e
faizankhandev <faizan480khan@gmail.com>
2026-02-21 21:26:36 +0500

    FIXED: the DialTo

--- app/api/twiml/route.js
+++ app/api/twiml/route.js
@@ -1,13 +1,13 @@
 import { twiml as TwilioTwiml } from "twilio";
 
-const myTwilioNumber = "+16268871097"; 
+const myTwilioNumber = "+16268871097";
 
 export async function POST(req) {
     const response = new TwilioTwiml.VoiceResponse();
     const bodyText = await req.text();
     const params = new URLSearchParams(bodyText);
-    const targetNumber = params.get("To") || params.get("dialTo");
-    response.dial({ callerId: myTwilioNumber }).number("+923162177746");
+    const targetNumber = String(params.get("To") || params.get("dialTo"));
+    response.dial({ callerId: myTwilioNumber }).number(targetNumber);
     return new Response(response.toString(), {
         headers: { "Content-Type": "text/xml" },
     });
